5ce2e5205e7c9f1cb82554f8b49fe89e189f2509
Racheal Kuranchie <kuranchieracheal@gmail.com>
2022-06-06 17:27:01 +0000

    homepage done

---
+++ src/Controllers/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='footer'>
+        <footer className="grid grid-cols-1 gap-0   md:grid-cols-2 place-content-center   ">
+            <div>
+                <h3>Email</h3>
+                <p>kuranchieracheal@gmail.com</p>
+            </div>
+
+            
+
+            <div>
+                <h3>Phone</h3>
+                <p>+233 (0) 542 802959</p>
+            </div>
+        </footer>
+    </div>
+  )
+}
--- src/Pages/index.js
+++ src/Pages/index.js
@@ -6,6 +6,7 @@ import {  Row, Col, Container } from "react-bootstrap";
 import Navigation from "../Controllers/Navigation";
 import Example from "../Controllers/Navigation";
 import image from '../images/pic1.jpg'
+import Footer from "../Controllers/Footer";
 import image2 from '../images/pic2.png'
 
 export default function Index() {
@@ -17,22 +18,28 @@ export default function Index() {
               {/* <Example/> */}
               <Navbarr />
 
-              <div className="header flex-1 md:flex py-4 px-4 md:py-8">
+              <div className="header flex-1 md:flex py-4 px-4 md:py-8 justify-around m-8">
                 
-                  <div className="text basis-1/2 md:basis-1/2  ">
-                    <h2 className="md:w-96 w-full">
+                  <div className="text basis-1/2 md:basis-1/2 items-center justify-center text-center md:text-left  ">
+                    <h2 className=" w-full">
                       {" "}
-                      Hello,{" "}
-                      <span>I’m Racheal Kuranchie, A software developer</span>
+                      Hello, {" "}<br></br>
+                      <span>I’m Racheal Kuranchie<br></br>  <span> A Software Engineer.</span></span>
                     </h2>
+
+                    <div>
+                      <a href="https://medium.com/@rachealkuranchie" className="icon p-3"><i class="fa-brands fa-linkedin"></i></a>
+                      <a href="https://medium.com/@rachealkuranchie" className="icon p-3"><i class="fa-brands fa-twitter"></i></a>
+                      <a href="https://medium.com/@rachealkuranchie" className="icon p-3"><i class="fa-brands fa-github"></i></a>
+                      <a href="https://medium.com/@rachealkuranchie" className="icon p-3"><i class="fa-brands fa-medium"></i></a>
+                   
+                    </div>
                     <button>Hire me</button>
                   </div>
                
-
-                
-                  <div className="img flex w-1/3 h-full">
+                  <div className="img basis-1/4 md:basis-1/3 items-center justify-center   p-0 md:w-full  ">
                     
-                    <img src={image2} alt="picture" className="h-96"></img>
+                    <img src={image2} alt="picture" className="h-96 w-full md:w-full  justify-center"></img>
                     
                     
                   </div>
@@ -41,14 +48,98 @@ export default function Index() {
             </header>
           
       {/* </Container> */}
+      <section className="flex-1 md:flex py-4 px-4 md:py-8 justify-around">
+        <div className="basis-1/4 md:basis-1/3 md:w-1/4  md:basis-1/2 bg-image m-8 ">
+        <img src={image} alt='pic' className="w-full"></img>  
+        </div>
 
-      <section>
+        <div className=" basis-1/2 md:basis-1/2  ">
+        <h2 className="font-bold ">About</h2>
+                <p>
+                Lörem ipsum heterov lynat paramun. Gälig poprens etira på anas: mingen.
+                 Bek kokrosamma infrar. Ynt äktigt isk. Pon favis panektig. Soliga astroling att mikrosk i influerare. 
+            <br></br>
+            Lad monodat wikiläcka. Spedigisk vetretest i raren fön och nysogisk. Or hyposamma 
+            spest. Byfaktigt oliga bebagt vektigt.
+                 <br></br>
+                Kagon beck koscheria kans för kropp. Stenons nyfägen bekagon semick. Benerar 
+                sobarade de nyv laras sarade. 
+
+                </p>
+                <button className="btns">Read more</button>
+        </div>
+        
+      </section>
+
+      {/* <section>
           <About/>
+      </section> */}
+
+      <section className="justify-center items-center projects" >
+        <h2 className="font-bold ">Projects</h2>
+        <div className="grid grid-cols-1 md:grid-cols-3 place-content-center m-8">
+
+        <div className="bg-images space-x-2 space-y-4  md:space-x-8">
+        <img src={image2} alt="picture" className="h-96 w-full md:w-full  justify-center"></img>
+        <button className="btns hover:bg-fuchsia-700">View Project</button>
+        </div>
+
+        <div className="bg-images space-x-2 space-y-4  md:space-x-8 ">
+          
+        <img src={image2} alt="picture" className="h-96 w-full  md:w-full  justify-center"></img>
+        <button className="btns hover:bg-fuchsia-700">View Project</button>
+        </div>
+
+        <div className="bg-images  space-x-2 space-y-4  md:space-x-8">
+        <img src={image2} alt="picture" className=" h-96 w-full md:w-full  justify-center "></img>
+        <button className="btns hover:bg-fuchsia-700">View Project</button>
+        </div>
+        </div>
+
+        <button className="btnz hover:">View More</button>
+
       </section>
 
-      <section>
-          <Projects/>
+      <section className="projects-section text-center">
+      <h2 className="font-bold ">Skills</h2>
+        <div className="grid grid-cols-2 gap-4 grid-rows-4 md:grid-cols-2 place-content-center m-2">
+
+            <li className="dot">JavaScript(Node.js)</li>
+
+            <li className="dot">MySQL & PostgreSQL</li>
+            
+            <li className="dot">HTML & CSS</li>
+
+            <li className="dot">Mongo DB</li>
+            
+            
+            <li className="dot">Git & GitHub</li>
+            
+            <li className="dot">React.js</li>
+        
+        </div>
       </section>
+
+      <section className=" download justify-center items-center">
+        <div className="grid grid-cols-2 gap-0   md:grid-cols-2 place-content-center m-8   ">
+          <div>
+          <button className="btnz hover:">Download CV</button>
+          </div>
+          <div>
+          <button className="btnz hover:">Get in Touch</button>
+          </div>
+        
+       
+        </div>
+       
+      </section>
+
+<section>
+  <Footer/>
+</section>
+      {/* <section>
+          <Projects/>
+      </section> */}
     </div>
   );
 }
