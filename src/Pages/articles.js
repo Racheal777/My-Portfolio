6099efb3ed63183ebef1ce9b14b16c9dab555bfc
Racheal Kuranchie <kuranchieracheal@gmail.com>
2022-06-17 19:56:47 +0000

    article page in progress

--- src/Pages/articles.js
+++ src/Pages/articles.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbarr from '../Controllers/Navbar'
+import Footer from "../Controllers/Footer";
 
 export default function Artcles() {
   return (
@@ -8,6 +9,17 @@ export default function Artcles() {
 <header>
     <Navbarr/>
 </header>
+
+<section className='flex md:flex-1'>
+  <div>
+    <h2>Authentication with sequelize, postgreSQL and express</h2>
+    <h3>lorem fyfcgveyr efrcgeysgcf ergceruydgcyu</h3>
+    
+  </div>
+
+</section>
+
+<Footer/>
     </div>
   )
 }
